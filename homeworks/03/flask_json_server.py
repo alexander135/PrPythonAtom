c91ae11ffc0fd589c07822b5d43ba5097e19dd70
alexander135 <oleshko-alex@mail.ru>
2018-10-31 18:05:26 +0300

    hw3

--- homeworks/03/flask_json_server.py
+++ homeworks/03/flask_json_server.py
@@ -7,11 +7,18 @@ def return_classifier_result(version):
     #TODO прочитать из полученного запроса json-контент
     #В случае, если version==1, то должен вернуться json с версией и полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
     #В случае, если version==0, то должен вернуться json с версией и полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
+    if version == 1:
+        return jsonify({'version':1,'predict':request.json['predict']})
+    else:
+        return jsonify({'version':0,'predict':request.json['old_predict']})
+    
+                    
+        
 
 @app.route("/")
 def hello():
     #TODO должна возвращатьс инструкция по работе с сервером
-    return
+    return 'instruction'
 
 if __name__ == "__main__":
     app.run()
