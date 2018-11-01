cadf23d5a8b8757d842044affb2beee243fbebaf
alexander135 <oleshko-alex@mail.ru>
2018-11-01 13:16:57 +0300

    all done hw_03

--- homeworks/03/.ipynb_checkpoints/flask_json_server-checkpoint.py
+++ homeworks/03/.ipynb_checkpoints/flask_json_server-checkpoint.py
@@ -7,10 +7,11 @@ def return_classifier_result(version):
     #TODO прочитать из полученного запроса json-контент
     #В случае, если version==1, то должен вернуться json с версией и полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
     #В случае, если version==0, то должен вернуться json с версией и полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
-    if version == 1:
-        return jsonify({'version':1,'predict':request.json['predict']})
-    else:
-        return jsonify({'version':0,'predict':request.json['old_predict']})
+    if request.method == 'POST':
+        if version == '1':
+            return jsonify({'version':1,'predict':request.json['predict']})
+        else:
+            return jsonify({'version':0,'predict':request.json['old_predict']})
     
                     
         
